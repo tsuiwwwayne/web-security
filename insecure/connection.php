00b6f222fb27039533e8fec98172aca7c86d862b
now-raymond <rayjta@gmail.com>
2017-01-10 20:10:12 +0000

    Update DB configuration

--- insecure/connection.php
+++ insecure/connection.php
@@ -6,7 +6,7 @@
     
     public static function getInstance() {
         if (!isset(self::$instance)) {
-            self::$instance = new PDO("mysql:host=localhost;dbname=scenario_3", "root", "");
+            self::$instance = new PDO("mysql:host=scenario3instance.cn23ibbreyu5.eu-west-1.rds.amazonaws.com;dbname=scenario_3", "scenario3_user", "scenario3_pass");
         }
         return self::$instance;
     }
