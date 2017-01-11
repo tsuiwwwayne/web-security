9583eb0d9f03b3c530c1a1c2e8c5b345a5fabfa9
Christopher Lau Min Yi <zcabmyl@ucl.ac.uk>
2017-01-11 20:42:53 +0000

    added profile

--- insecure/controllers/profile_controller.php
+++ insecure/controllers/profile_controller.php
@@ -5,7 +5,26 @@
       if (!isset($_SESSION['user_id'])) {
           return call('pages', 'error');
       }
-      $username = User::getUsername($_SESSION['user_id']);
+      $updated = 0; 
+      $profile = Profile::getProfile($_SESSION['user_id']);
+      require_once('views/profile/index.php');
+    }
+    public function updateProfile(){
+      if (!isset($_SESSION['user_id'])) {
+          return call('pages', 'error');
+      }
+      require_once('models/profile.php');
+      //$username = User::getUserName($_SESSION['user_id']);
+      $user_id = $_SESSION['user_id'];
+      $displayName = $_POST["display_name"];
+      $icon = $_POST["icon"];
+      $homepage = $_POST["homepage"];
+      $profile_colour = $_POST["profile_colour"];
+
+      Profile::updateProfile($user_id, $displayName, $icon, $homepage, $profile_colour);
+
+      $profile = Profile::getProfile($_SESSION['user_id']);
+      $updated = 1;
       require_once('views/profile/index.php');
     }
   }
