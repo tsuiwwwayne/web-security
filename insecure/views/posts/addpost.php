6f7b437896d1f0127b3dfd32a1fb607ae4f91328
Christopher Lau Min Yi <zcabmyl@ucl.ac.uk>
2017-01-11 23:59:09 +0000

    fix layout of add post

--- insecure/views/posts/addpost.php
+++ insecure/views/posts/addpost.php
@@ -1,14 +1,16 @@
-<div class="panel panel-default" style="margin-top: 30px;">
-  <div class="panel-heading">New Post</div>
-  <div class="panel-body">
-      <form class="form-horizontal" action="?controller=posts&action=add" method="post" enctype="multipart/form-data">
-        <div class="form-group">
-         	<label class="control-label col-sm-2" for="pwd">Add a new post:</label>
-          <div class="col-sm-10"> 
-            <textarea class="form-control" rows="5"  name="content"></textarea><br>
-            <button type="submit" class="btn btn-default">Add Post</button>
+<div class="container below-static-navbar padding-top-20">
+  <div class="panel panel-default" style="margin-top: 30px;">
+    <div class="panel-heading">New Post</div>
+    <div class="panel-body">
+        <form class="form-horizontal" action="?controller=posts&action=add" method="post" enctype="multipart/form-data">
+          <div class="form-group">
+           	<label class="control-label col-sm-2" for="pwd">Add a new post:</label>
+            <div class="col-sm-10"> 
+              <textarea class="form-control" rows="5"  name="content"></textarea><br>
+              <button type="submit" class="btn btn-default">Add Post</button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+    </div>
   </div>
 </div>
