dd3c6604ec400e9b526d3d723d71caa52e4c9db7
now-raymond <rayjta@gmail.com>
2017-01-12 16:55:35 +0000

    Make CSRF token generation more concise.

--- insecure/index.php
+++ insecure/index.php
@@ -8,11 +8,7 @@
   // CSRF token generation
   if (WEB_SAFE) {
     if (empty($_SESSION['token'])) {
-      if (function_exists('mcrypt_create_iv')) {
-        $_SESSION['token'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
-      } else {
-        $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(32));
-      }
+      $_SESSION['token'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
     }
   } else {
     unset($_SESSION['token']);
