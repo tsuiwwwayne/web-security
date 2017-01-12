a1fbe74196a0ee094ef5b365a8971335e3272831
Christopher Lau Min Yi <zcabmyl@ucl.ac.uk>
2017-01-12 16:40:32 +0000

    fixed error page

--- insecure/views/pages/error.php
+++ insecure/views/pages/error.php
@@ -1,6 +1,8 @@
-<p>Oops, this is the error page.</p>
 
-<p>Looks like something went wrong.</p>
-<?php if (isset($output)): ?>
-<p>Please review the following error message: <?php echo $output ?></p>
-<?php endif ?>
+<div class="container below-static-navbar padding-top-20">
+	<p>Oops, this is the error page.</p>
+	<p>Looks like something went wrong.</p>
+	<?php if (isset($output)): ?>
+		<p>Please review the following error message: <?php echo $output ?></p>
+	<?php endif ?>
+</div>
