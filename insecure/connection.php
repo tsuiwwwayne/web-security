129b9ae31b263ae90f0047503cf2cf563021b5b6
now-raymond <rayjta@gmail.com>
2017-01-15 16:29:00 +0000

    Changed to Azure MySQL DB from Amazon

--- insecure/connection.php
+++ insecure/connection.php
@@ -6,7 +6,14 @@
 
     public static function getInstance() {
         if (!isset(self::$instance)) {
-            self::$instance = new PDO("mysql:host=scenario3instance.cn23ibbreyu5.eu-west-1.rds.amazonaws.com;dbname=scenario_3", "scenario3_user", "scenario3_pass");
+            $db_host = "eu-cdbr-azure-west-a.cloudapp.net";
+            $db_name = "scenariodb3";
+            $db_user = "bedcdb8cd4f6ec";
+            $db_pass = "7682fd8d";
+          
+            //self::$instance = new PDO("mysql:host=scenario3instance.cn23ibbreyu5.eu-west-1.rds.amazonaws.com;dbname=scenario_3", "scenario3_user", "scenario3_pass");
+            self::$instance = new PDO("mysql:host=eu-cdbr-azure-west-a.cloudapp.net;dbname=scenariodb3", "bedcdb8cd4f6ec", "7682fd8d");
+            //self::$instance = new PDO("mysql:host=$db_host;dbname=$db_name", "$db_user", "$db_pass");
         }
         return self::$instance;
     }
